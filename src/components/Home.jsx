e27a39e663780a5cdd4682362cbfa3ade07f635c
Sujit Rao <sujitrao867@gmail.com>
2024-05-09 21:28:37 +0530

    changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,7 +20,7 @@ const refresh = () => {
     const formData = new FormData()
     formData.append("file", selectedFile)
     try {
-      const response = await axios.post("https://word-to-pdf-backend.vercel.app/convertFile", formData, {
+      const response = await axios.post("http://localhost:3000/convertFile", formData, {
         responseType: "blob",
       });
 
